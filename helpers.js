5e684539b33adf994629530034e99be1fb9523ca
Joshua Tuddenham <joshuaisaact@gmail.com>
2024-08-11 08:35:50 +0100

    Added animations, responsive design, embedded trailer, IMDB link

---
+++ helpers.js
@@ -0,0 +1,157 @@
+// Try and catch the API
+
+
+// Populate dropdown menu with all the available genres
+const populateGenreDropdown = (genres) => {
+  const select = document.getElementById('genres')
+
+  for (const genre of genres) {
+    let option = document.createElement("option");
+    option.value = genre.id;
+    option.text = genre.name;
+    select.appendChild(option);
+  }
+};
+
+// Returns the current genre selection from the dropdown menu
+const getSelectedGenre = () => {
+  const selectedGenre = document.getElementById('genres').value;
+  return selectedGenre;
+};
+
+// Clear the current movie from the screen
+const clearCurrentMovie = () => {
+  const moviePosterDiv = document.querySelector('.moviePoster');
+  const movieTextDiv = document.querySelector('.movieDetails');
+  moviePosterDiv.innerHTML = '';
+  movieTextDiv.innerHTML = '';
+}
+
+// After liking a movie, clears the current movie from the screen and gets another random movie
+const likeMovie = () => {
+  clearCurrentMovie();
+  showRandomMovie();
+};
+
+// After disliking a movie, clears the current movie from the screen and gets another random movie
+const dislikeMovie = () => {
+  clearCurrentMovie();
+  showRandomMovie();
+};
+
+// Create HTML for movie poster
+const createMoviePoster = (posterPath) => {
+  const moviePosterUrl = `https://image.tmdb.org/t/p/original/${posterPath}`;
+
+  const posterImg = document.createElement('img');
+  posterImg.setAttribute('src', moviePosterUrl);
+  posterImg.setAttribute('class', 'moviePoster');
+
+  return posterImg;
+};
+
+// Create HTML for movie title
+const createMovieTitle = (title) => {
+  const titleHeader = document.createElement('h1');
+  titleHeader.setAttribute('class', 'movieTitle');
+  titleHeader.innerHTML = title;
+
+  return titleHeader;
+};
+
+// Create HTML for release date
+const createReleaseDate = isoDate => {
+  const date = new Date(isoDate);
+
+  const day = date.getDate();
+  const month = date.toLocaleString('default', { month: 'long' });
+  const year = date.getFullYear();
+
+
+  const relDate = document.createElement('p')
+  relDate.setAttribute('class', 'relDate')
+  relDate.innerHTML = `Released: ${day} ${month} ${year}`
+
+  return relDate;
+}
+
+// Create HTML for movie overview
+const createMovieOverview = (overview) => {
+  const overviewParagraph = document.createElement('p');
+  overviewParagraph.setAttribute('id', 'movieOverview');
+  overviewParagraph.innerHTML = overview;
+  return overviewParagraph;
+};
+
+// Create HTML for IMDB Link
+const createIMDBLink = imdb => {
+  const imdbL = document.createElement('a');
+  imdbL.setAttribute('href', `https://www.imdb.com/title/${imdb}`)
+  imdbL.textContent = 'IMDB';
+  imdbL.classList.add('imdb')
+  return imdbL;
+}
+
+// Create HTML for streaming platforms
+const createStreamingList = list => {
+  const streamingList = document.createElement('ul')
+  streamingList.classList.add('streamList');
+  list['flatrate'].forEach(el => {
+    let item = document.createElement('li');
+    item.innerHTML = `<img src=https://media.themoviedb.org/t/p/original/${el['logo_path']}><br>${el['provider_name']}`;
+    streamingList.appendChild(item);
+  })
+  return streamingList;
+}
+
+// Create HTML for embedded trailers
+const createTrailerVideos = vidArr => {
+  const trailerVideos = document.createElement('div')
+  trailerVideos.classList.add('trailer')
+  vidArr.forEach(el => {
+    trailerVideos.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${el.key}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard - write; encrypted - media; gyroscope; picture -in -picture; web - share" referrerpolicy="strict - origin - when - cross - origin" allowfullscreen></iframe>`
+  })
+  return trailerVideos;
+}
+
+// Returns a random movie from the page of movies
+const getRandomMovie = (movies) => {
+  const randomIndex = Math.floor(Math.random() * movies.length);
+  const randomMovie = movies[randomIndex];
+  return randomMovie;
+};
+
+// Uses the DOM to create HTML to display the movie
+const displayMovie = (movieInfo, streamInfo, trailer) => {
+  const moviePosterDiv = document.querySelector('.moviePoster');
+  const movieTextDiv = document.querySelector('.movieDetails');
+
+  // Create HTML content containing movie info
+  const moviePoster = createMoviePoster(movieInfo.poster_path);
+  const titleHeader = createMovieTitle(movieInfo.title);
+  const releaseDate = createReleaseDate(movieInfo.release_date)
+  const overviewText = createMovieOverview(movieInfo.overview);
+  const IMDBlink = createIMDBLink(movieInfo.imdb_id)
+  const streamingInfo = createStreamingList(streamInfo)
+  const trailerVideos = createTrailerVideos(trailer)
+
+  // Append title, poster, and overview to page
+  moviePosterDiv.appendChild(moviePoster);
+  movieTextDiv.appendChild(titleHeader);
+  movieTextDiv.appendChild(releaseDate);
+  movieTextDiv.appendChild(overviewText);
+  movieTextDiv.appendChild(IMDBlink)
+  movieTextDiv.appendChild(streamingInfo);
+  movieTextDiv.appendChild(trailerVideos)
+
+};
+
+// const streamingInfo = (streaming) => {
+//   const streamingDiv = document.querySelector('.streamingPlatforms')
+//   const movieTextDiv = document.querySelector('.movieText');
+//   const streamingInfo = createStreamingList(streaming);
+
+//   streamingDiv.appendChild(streamingInfo);
+
+
+// }
--- script.js
+++ script.js
@@ -1,2 +1,129 @@
 const mykey = config.MY_KEY;
 const secretkey = config.SECRET_KEY;
+const tmdbBaseUrl = 'https://api.themoviedb.org/3/'
+
+const playBtn = document.querySelector('.playBtn');
+
+// Pull a list of genres from the API
+const getGenres = async () => {
+  const genreRequestEndpoint = 'genre/movie/list';
+  const requestParams = `?api_key=${secretkey}`
+  const urlToFetch = tmdbBaseUrl + genreRequestEndpoint + requestParams;
+
+  try {
+    const response = await fetch(urlToFetch)
+    if (response.ok) {
+      const jsonresponse = await response.json();
+      const genres = await jsonresponse.genres;
+      return genres;
+    }
+  }
+  catch (error) {
+    console.log(error);
+  }
+};
+
+// Pull a list of movies from the API (random page from 1 to 100)
+const getMovies = async () => {
+  const selectedGenre = getSelectedGenre();
+  const discoverMovieEndpoint = 'discover/movie';
+  const requestParams = `?api_key=${secretkey}&with_genres=${selectedGenre}&region=GB&page=${Math.trunc(Math.random() * 100) + 1}`;
+  const urlToFetch = tmdbBaseUrl + discoverMovieEndpoint + requestParams;
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const jsonresponse = await response.json();
+      const movies = jsonresponse.results;
+      console.log(movies)
+      return movies;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Grab detailed information about a random film
+const getMovieInfo = async (movie) => {
+  const movieID = movie.id;
+  const movieEndpoint = `movie/${movieID}`;
+  const requestParams = `?api_key=${secretkey}`;
+  const urlToFetch = `${tmdbBaseUrl}${movieEndpoint}${requestParams}`;
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const movieInfo = await response.json();
+      console.log(movieInfo);
+      return movieInfo;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Get a film trailer link from the Videos endpoint
+const getTrailer = async (movie) => {
+  const movieID = movie.id;
+  const movieEndpoint = `movie/${movieID}/videos`;
+  const requestParams = `?api_key=${secretkey}`;
+  const urlToFetch = `${tmdbBaseUrl}${movieEndpoint}${requestParams}`;
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      let trailerInfo = await response.json();
+      trailerInfo = trailerInfo.results;
+      console.log(trailerInfo);
+      return trailerInfo;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Get streaming platform availability information from a different endpoint
+const getStreamingPlatforms = async (movie) => {
+  const movieID = movie.id;
+  const movieEndpoint = `movie/${movieID}/watch/providers`;
+  const requestParams = `?api_key=${secretkey}`;
+  const urlToFetch = `${tmdbBaseUrl}${movieEndpoint}${requestParams}`;
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const streamingInfo = await response.json();
+      console.log(streamingInfo.results['GB'])
+      return streamingInfo;
+    }
+  } catch (error) {
+    showRandomMovie();
+  }
+}
+
+
+// Gets a list of movies and ultimately displays the info of a random movie from the list, that has free streaming
+const showRandomMovie = async () => {
+  const movieInfo = document.querySelector('.movieInfo');
+  if (movieInfo.childNodes.length > 0) {
+    clearCurrentMovie();
+  };
+  const movies = await getMovies();
+  const randomMovie = getRandomMovie(movies);
+  const info = await getMovieInfo(randomMovie);
+  const trailer = await getTrailer(randomMovie)
+  const streaming = await getStreamingPlatforms(randomMovie)
+
+  if (streaming && streaming.results && streaming.results['GB'] && streaming.results['GB'].flatrate) {
+    displayMovie(info, streaming.results['GB'], trailer);
+  } else {
+    console.log('No flatrate streaming info available, retrying...');
+    // if (retryCount < MAX_RETRIES) {
+    showRandomMovie(); // Retry if flatrate is undefined
+    // } else {
+    //   console.log('Max retries reached. Could not find a movie with flatrate streaming.');
+    // }
+  }
+
+  // displayMovie(info, streaming.results['GB']);
+  // streamingInfo(streaming.results['GB']);
+};
+
+getGenres().then(populateGenreDropdown);
+playBtn.onclick = showRandomMovie;
